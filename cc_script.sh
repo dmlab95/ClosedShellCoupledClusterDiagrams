2adb9369b94222a9e3c6f63a138b66513eb883b4
dmlab95 <37669962+dmlab95@users.noreply.github.com>
2018-12-05 11:40:17 +0530

    Add files via upload

---
+++ cc_script.sh
@@ -0,0 +1,34 @@
+
+
+
+#==================================================================================================
+
+#Run automatic ccsd/ccsdt amplitude terms generator python program 
+#closed shell, orbital basis
+#from orbital based spinfree Wick's theorem
+#which stores final result in result.tex
+#in latex input mode
+#get detailed output along with final terms in ans
+#change latex.py for specific output format
+
+#==================================================================================================
+
+    echo 
+    read -p "For Closed Shell CCSD diagrams give input as 1, for CCSDT give 2:   " prompt
+    case $prompt in
+        1) echo 'Here are the diagrams for CCSD'; sed -i 's/"CCSDT"/"CCSD"/g' driver.py;;
+        2) echo 'Here are the diagrams for CCSDT'; sed -i 's/"CCSD"/"CCSDT"/g' driver.py;;
+        * ) echo "Please answer 1 or 2, run the script again"; exit;;
+    esac
+
+python driver.py > ans
+
+
+#Convert: result.tex --> result.pdf
+
+pdflatex result.tex &&
+
+
+#Open result.pdf on terminal
+
+evince result.pdf 
